b3a06f3e02106846f0a0fbaa78ecfdc4fc793965
jyothsnaguravana <121102225+jyothsnaguravana@users.noreply.github.com>
2023-10-27 16:59:06 +0530

    Add files via upload

---
+++ TODO_LIST/index.js
@@ -0,0 +1,36 @@
+import express from "express" ;  
+import bodyParser from "body-parser";  
+
+
+const app = express(); 
+
+app.use(express.static("public")); 
+app.use(bodyParser.urlencoded({extended : true}));  
+
+const items = ["do something ", "never say no"] ; 
+
+app.get("/" ,(req,res) => {
+    res.render("index.ejs" ,{items}); 
+}) 
+
+
+
+app.post("/insert" , (req ,res) => { 
+    const item = req.body["item"];
+    items.push(item); 
+    const n = items.length; 
+    // console.log(n);
+    res.render("index.ejs" ,{items}); 
+}) 
+
+app.post("/delete/:index" , (req,res) => { 
+    const b = req.params.index; 
+    // console.log(b);
+    items.splice(b,1) ;
+    res.render("index.ejs" ,{items});
+})
+
+
+app.listen( 3000 , () => {
+    console.log("You are being served"); 
+})
